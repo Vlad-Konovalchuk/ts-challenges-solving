abff034389d881ddbd0678b22c41bc9f2f3ab9a1
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-07-28 18:53:56 +0300

    solved easy pick challenge

---
+++ 00004-easy-pick.ts
@@ -0,0 +1,33 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils';
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
+
+type A = 'title' | 'completed';
+type B = {
+  [key in A]: '';
+};
+// ============= Your Code Here =============
+type MyPick<T, K extends keyof T> = {
+  [Key in K]: T[Key];
+};
